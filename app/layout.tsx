bf21dbd0463eacdf3e9ebd7de962d29e1daa26e8
afsun7 <omidi.afsaneh7@gmail.com>
2026-01-30 19:05:22 +0330

    add dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,12 +7,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html dir="rtl" className={`${figtree.variable} ${vazir.variable}`}>
-      <body className="flex flex-col min-h-screen font-bold uppercase">
+    <html dir="rtl" className={`dark ${figtree.variable} ${vazir.variable}`}>
+      <body className=" flex flex-col min-h-screen font-bold uppercase">
         <header className="bg-gray-200 flex items-center justify-center text-3xl h-20">
           دوره معماری ری اکت
         </header>
-        <div className="flex-1 flex  justify-center items-center">
+        <div className="flex-1 flex  justify-center items-center dar: bg-base-100 dark: text-base-content">
           {children}
         </div>
         <footer className="bg-gray-200 flex items-center justify-center text-3xl h-20">
